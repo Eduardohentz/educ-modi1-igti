79d62bc69a0a96b15a9da42edf9a14f981ed6105
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2022-05-09 23:24:38 -0300

    Ajusta arquivo s3_files.tf

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -10,14 +10,14 @@ resource "aws_s3_object" "delta_insert" {
   bucket = aws_s3_bucket.datalake.id
   key    = "emr-code/pyspark/01_delta_spark_insert.py"
   acl    = "private"
-  source = "../01_delta_spark_insert.py"
-  etag   = filemd5("../01_delta_spark_insert.py")
+  source = "../etl/01_delta_spark_insert.py"
+  etag   = filemd5("../etl/01_delta_spark_insert.py")
 }
 
 resource "aws_s3_object" "delta_upsert" {
   bucket = aws_s3_bucket.datalake.id
   key    = "emr-code/pyspark/02_delta_spark_upsert.py"
   acl    = "private"
-  source = "../02_delta_spark_upsert.py"
-  etag   = filemd5("../02_delta_spark_upsert.py")
+  source = "../etl/02_delta_spark_upsert.py"
+  etag   = filemd5("../etl/02_delta_spark_upsert.py")
 }
