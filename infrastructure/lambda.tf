cc0c26b7f1140c79e5b7c6bca07b3244552e7ad5
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2022-05-09 23:33:11 -0300

    FIx file lambda terraform

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "executa_emr" {
 
   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "python3.10.4"
+  runtime = "python3.8"
 
   # tags {
   #   IES   = "IGTI"
