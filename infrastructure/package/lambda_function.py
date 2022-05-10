793cfa3d6d8e89246c72d24ea7e28861104bb42d
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2022-05-10 17:44:17 -0300

    Ajuste script bash zipar lambda function

--- infrastructure/package/lambda_function.py
+++ infrastructure/package/lambda_function.py
@@ -2,5 +2,119 @@ import boto3
 
 def handler(event, context):
     """
-    Lambda function that starts a job flow in EMR
+    Lambda function that starts a job flow in EMR.
     """
+    client = boto3.client('emr', region_name='us-east-1')
+
+    cluster_id = client.run_job_flow(
+                Name='EMR-Jorge-IGTI-delta',
+                ServiceRole='EMR_DefaultRole',
+                JobFlowRole='EMR_EC2_DefaultRole',
+                VisibleToAllUsers=True,
+                LogUri='s3://datalake-jorge-igti-tf-producao-431738431676/emr-logs',
+                ReleaseLabel='emr-6.3.0',
+                Instances={
+                    'InstanceGroups': [
+                        {
+                            'Name': 'Master nodes',
+                            'Market': 'SPOT',
+                            'InstanceRole': 'MASTER',
+                            'InstanceType': 'm5.xlarge',
+                            'InstanceCount': 1,
+                        },
+                        {
+                            'Name': 'Worker nodes',
+                            'Market': 'SPOT',
+                            'InstanceRole': 'CORE',
+                            'InstanceType': 'm5.xlarge',
+                            'InstanceCount': 1,
+                        }
+                    ],
+                    'Ec2KeyName': 'jorge-igti-teste',
+                    'KeepJobFlowAliveWhenNoSteps': True,
+                    'TerminationProtected': False,
+                    'Ec2SubnetId': 'subnet-1df20360'
+                },
+
+                Applications=[
+                    {'Name': 'Spark'},
+                    {'Name': 'Hive'},
+                    {'Name': 'Pig'},
+                    {'Name': 'Hue'},
+                    {'Name': 'JupyterHub'},
+                    {'Name': 'JupyterEnterpriseGateway'},
+                    {'Name': 'Livy'},
+                ],
+
+                Configurations=[{
+                    "Classification": "spark-env",
+                    "Properties": {},
+                    "Configurations": [{
+                        "Classification": "export",
+                        "Properties": {
+                            "PYSPARK_PYTHON": "/usr/bin/python3",
+                            "PYSPARK_DRIVER_PYTHON": "/usr/bin/python3"
+                        }
+                    }]
+                },
+                    {
+                        "Classification": "spark-hive-site",
+                        "Properties": {
+                            "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+                        }
+                    },
+                    {
+                        "Classification": "spark-defaults",
+                        "Properties": {
+                            "spark.submit.deployMode": "cluster",
+                            "spark.speculation": "false",
+                            "spark.sql.adaptive.enabled": "true",
+                            "spark.serializer": "org.apache.spark.serializer.KryoSerializer"
+                        }
+                    },
+                    {
+                        "Classification": "spark",
+                        "Properties": {
+                            "maximizeResourceAllocation": "true"
+                        }
+                    }
+                ],
+                
+                StepConcurrencyLevel=1,
+                
+                Steps=[{
+                    'Name': 'Delta Insert do ENEM',
+                    'ActionOnFailure': 'CONTINUE',
+                    'HadoopJarStep': {
+                        'Jar': 'command-runner.jar',
+                        'Args': ['spark-submit',
+                                 '--packages', 'io.delta:delta-core_2.12:1.0.0', 
+                                 '--conf', 'spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension', 
+                                 '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
+                                 '--master', 'yarn',
+                                 '--deploy-mode', 'cluster',
+                                 's3://datalake-jorge-igti-tf-producao-431738431676/emr-code/pyspark/01_delta_spark_insert.py'
+                                 ]
+                    }
+                },
+                {
+                    'Name': 'Simulacao e UPSERT do ENEM',
+                    'ActionOnFailure': 'CONTINUE',
+                    'HadoopJarStep': {
+                        'Jar': 'command-runner.jar',
+                        'Args': ['spark-submit',
+                                 '--packages', 'io.delta:delta-core_2.12:1.0.0', 
+                                 '--conf', 'spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension', 
+                                 '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
+                                 '--master', 'yarn',
+                                 '--deploy-mode', 'cluster',
+                                 's3://datalake-jorge-igti-tf-producao-431738431676/emr-code/pyspark/02_delta_spark_upsert.py'
+                                 ]
+                    }
+                }],
+            )
+    
+    return {
+        'statusCode': 200,
+        'body': f"Started job flow {cluster_id['JobFlowId']}"
+    }
