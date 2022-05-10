1f0e3739555c86a973c9fa24d912e78883b010fa
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2022-05-10 01:48:43 -0300

    Correcao arquivo ZIP

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -73,5 +73,5 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "lambda_attach" {
   role       = aws_iam_role.lambda.name
-  policy_arn = aws_iam_role.lambda.arn
+  policy_arn = aws_iam_policy.lambda.arn
 }
